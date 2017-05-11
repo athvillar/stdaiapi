bf3dfa740a2fba0bf6c12223faf1e1b7651483d7
hanqing <hanq@zh.tc>
2017-05-12 02:31:11 +0800

    no class

--- stdaiapi-dao/src/main/java/cn/standardai/api/dao/DataDicDataDao.java
+++ stdaiapi-dao/src/main/java/cn/standardai/api/dao/DataDicDataDao.java
@@ -3,13 +3,13 @@ package cn.standardai.api.dao;
 import java.util.List;
 
 import cn.standardai.api.dao.bean.DataDicData;
-import cn.standardai.api.dao.bean.Image;
+//import cn.standardai.api.dao.bean.Image;
 
 public interface DataDicDataDao {
 
 	List<DataDicData> selectById(String dataDicId);
 
-	void insert(List<Image> param);
+	//void insert(List<Image> param);
 
 	void deleteByDatasetId(String dataDicId);
 }
