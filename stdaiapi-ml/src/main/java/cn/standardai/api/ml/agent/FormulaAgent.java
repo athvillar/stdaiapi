8ed741ba6f44d59cc66b41a53ee0f2b844a908f8
shihongfei <shihf@zh.tc>
2017-06-20 13:07:53 -1000

    ml uploadImage

--- stdaiapi-ml/src/main/java/cn/standardai/api/ml/agent/FormulaAgent.java
+++ stdaiapi-ml/src/main/java/cn/standardai/api/ml/agent/FormulaAgent.java
@@ -1,22 +1,59 @@
 package cn.standardai.api.ml.agent;
 
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.util.List;
 
-import com.alibaba.fastjson.JSONArray;
+import javax.imageio.ImageIO;
+
+import org.springframework.web.multipart.MultipartFile;
+
 import com.alibaba.fastjson.JSONObject;
 
 import cn.standardai.api.core.base.AuthAgent;
-import cn.standardai.api.core.exception.AuthException;
-import cn.standardai.api.ml.exception.MLException;
-import cn.standardai.api.ml.filter.FilterType;
+import cn.standardai.api.ml.exception.DataException;
 import cn.standardai.tool.literalImage.LiteralUtil;
 import cn.standardai.tool.literalImage.Word;
 
 public class FormulaAgent extends AuthAgent {
 
-	public Integer[][] parse(JSONObject request) {
-		// TODO
-		return null;
+	public Integer[][] parse(MultipartFile[] uploadfiles) throws DataException {
+		Integer[][] imagePixel = null;
+		if (uploadfiles != null && uploadfiles.length != 0) {
+			MultipartFile imageFile = uploadfiles[0];
+			ByteArrayInputStream byteInputStream = null;
+			BufferedImage bufferedImage = null;
+			try {
+				byteInputStream = new ByteArrayInputStream(imageFile.getBytes());
+				bufferedImage = ImageIO.read(byteInputStream);
+
+				int width = bufferedImage.getWidth();
+				int height = bufferedImage.getHeight();
+				imagePixel = new Integer[width][height];
+
+				for (int i = 0; i < width; i++) {
+					for (int j = 0; j < height; j++) {
+						int rgb = bufferedImage.getRGB(i, j);
+						int R = (rgb & 0xff0000) >> 16;
+						int G = (rgb & 0xff00) >> 8;
+						int B = (rgb & 0xff);
+						imagePixel[i][j] = (R + G + B) / 3;
+					}
+				}
+			} catch (IOException e) {
+				throw new DataException("读取图片数据异常", e);
+			} finally {
+				if (byteInputStream != null) {
+					try {
+						byteInputStream.close();
+					} catch (IOException e) {
+						throw new DataException("读取图片数据异常", e);
+					}
+				}
+			}
+		}
+		return imagePixel;
 	}
 
 	public JSONObject check(Integer[][] gray, String modelName) {
--- stdaiapi-ml/src/main/java/cn/standardai/api/ml/service/AppService.java
+++ stdaiapi-ml/src/main/java/cn/standardai/api/ml/service/AppService.java
@@ -5,18 +5,16 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.alibaba.fastjson.JSONObject;
 
 import cn.standardai.api.core.base.BaseService;
-import cn.standardai.api.core.exception.StdaiException;
-import cn.standardai.api.ml.agent.DnnAgent;
 import cn.standardai.api.ml.agent.FormulaAgent;
 
 @Controller
@@ -28,15 +26,13 @@ public class AppService extends BaseService {
 	private Logger logger = LoggerFactory.getLogger(AppService.class);
 
 	@RequestMapping(value = "/formula", method = RequestMethod.POST)
-	public String formula(@RequestHeader HttpHeaders headers, @RequestBody JSONObject request) {
+	public String formula(@RequestHeader HttpHeaders headers, @RequestParam("files") MultipartFile[] uploadfiles) {
 		logger.info("stdaiapi-ml /app/formula POST start ...");
 		JSONObject result = null;
 		FormulaAgent agent = new FormulaAgent();
 		try {
-			result = agent.check(agent.parse(request));
+			result = agent.check(agent.parse(uploadfiles), "");
 			result = successResponse(result);
-		} catch (StdaiException e) {
-			result = makeResponse(ReturnType.FAILURE, null, e.getMessage());
 		} catch (Exception e) {
 			e.printStackTrace();
 			result = makeResponse(ReturnType.FAILURE, null, e.getMessage());
